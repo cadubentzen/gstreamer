7034e05f5524109f3f8209a33d546e19597f15e6
Thibault Saunier <tsaunier@igalia.com>
2025-07-16 21:23:39 -0400

    pluginfeature: export the rank as a property
    This way users can get notified when it is changed.

--- subprojects/gstreamer/tests/check/gst/gstelementfactory.c
+++ subprojects/gstreamer/tests/check/gst/gstelementfactory.c
@@ -194,6 +194,48 @@ GST_START_TEST (test_class)
 
 GST_END_TEST;
 
+static gint rank_notify_count = 0;
+
+static void
+rank_notify_cb (GObject * object, GParamSpec * pspec, gpointer user_data)
+{
+  rank_notify_count++;
+}
+
+GST_START_TEST (test_plugin_feature_rank_property)
+{
+  GstPluginFeature *feature;
+  guint rank;
+
+  feature = g_object_new (GST_TYPE_ELEMENT_FACTORY, NULL);
+  fail_unless (feature != NULL);
+
+  g_object_get (feature, "rank", &rank, NULL);
+  fail_unless_equals_int (rank, GST_RANK_NONE);
+
+  g_signal_connect (feature, "notify::rank", G_CALLBACK (rank_notify_cb), NULL);
+
+  g_object_set (feature, "rank", GST_RANK_PRIMARY, NULL);
+  g_object_get (feature, "rank", &rank, NULL);
+  fail_unless_equals_int (rank, GST_RANK_PRIMARY);
+  fail_unless_equals_int (rank_notify_count, 1);
+  gst_plugin_feature_set_rank (feature, GST_RANK_SECONDARY);
+  fail_unless_equals_int (gst_plugin_feature_get_rank (feature),
+      GST_RANK_SECONDARY);
+  fail_unless_equals_int (rank_notify_count, 2);
+
+  gst_plugin_feature_set_rank (feature, GST_RANK_SECONDARY);
+  fail_unless_equals_int (rank_notify_count, 2);
+
+  gst_plugin_feature_set_rank (feature, GST_RANK_MARGINAL);
+  fail_unless_equals_int (gst_plugin_feature_get_rank (feature),
+      GST_RANK_MARGINAL);
+  fail_unless_equals_int (rank_notify_count, 3);
+
+  gst_object_unref (feature);
+}
+
+GST_END_TEST;
 
 static Suite *
 gst_element_factory_suite (void)
@@ -207,6 +249,7 @@ gst_element_factory_suite (void)
   tcase_add_test (tc_chain, test_element_factory);
   tcase_add_test (tc_chain, test_can_sink_any_caps);
   tcase_add_test (tc_chain, test_can_sink_all_caps);
+  tcase_add_test (tc_chain, test_plugin_feature_rank_property);
 
   return s;
 }

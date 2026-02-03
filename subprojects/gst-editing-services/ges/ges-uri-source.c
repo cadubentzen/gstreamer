02b40cc307744094af05cbc7e0ea855f88a9c7f4
Thibault Saunier <tsaunier@igalia.com>
2026-02-03 18:18:35 -0300

    Revert "WIP: Move to using the new rsvideoconvert element for software video conversion"
    This reverts commit b53d08ef65e3c7c9465bcdcdc93c53167668e492.

--- subprojects/gst-editing-services/ges/ges-uri-source.c
+++ subprojects/gst-editing-services/ges/ges-uri-source.c
@@ -652,7 +652,7 @@ ges_uri_source_create_uridecodepoolsrc (GESUriSource * self)
 
       } else {
         filter =
-            "rsvideoconvert ! capsfilter caps=\"video/x-raw,format=RGBA\" ! segmentclipper";
+            "videoconvert ! capsfilter caps=\"video/x-raw(ANY),format=RGBA\" ! segmentclipper";
       }
     }
     GST_DEBUG ("Using caps: %" GST_PTR_FORMAT " for uridecodepoolsrc", caps);

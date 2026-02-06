b2b346c5a85c8afc4f2a57b108c22222a7acf5ef
Thibault Saunier <tsaunier@igalia.com>
2026-02-06 19:31:12 +0000

    nlecomposition: fix race condition with seek_seqnum during concurrent seeks
    When a new seek arrives via _add_seek_action() while _seek_pipeline_func()
    is still processing a previous seek on the task thread, zeroing seek_seqnum
    in _add_seek_action() causes a race condition. The task thread's
    _set_current_bin_to_ready() would then create a FLUSH_START event with
    seqnum 0, which gets dropped by the ghost event probe (seqnum mismatch).
    Downstream elements then receive FLUSH_STOP without a preceding FLUSH_START,
    leading to pipeline hangs/timeouts.
    
    Fix this by:
    1. Removing the seek_seqnum = 0 reset from _add_seek_action(), since the
       seqnum will be properly set in _seek_pipeline_func() when the new seek
       action is actually executed on the task thread.
    2. Using flush_seqnum instead of seek_seqnum in _set_current_bin_to_ready()
       for the FLUSH_START event, since both are set to the same value in
       _seek_pipeline_func() and flush_seqnum is semantically correct for
       flush events.
    
    Fixes #4020

--- subprojects/gst-editing-services/plugins/nle/nlecomposition.c
+++ subprojects/gst-editing-services/plugins/nle/nlecomposition.c
@@ -1133,7 +1133,6 @@ _add_seek_action (NleComposition * comp, GstEvent * event)
 
   comp->priv->next_eos_seqnum = 0;
   comp->priv->real_eos_seqnum = 0;
-  comp->priv->seek_seqnum = 0;
   _add_action_locked (comp, G_CALLBACK (_seek_pipeline_func), seekd,
       G_PRIORITY_DEFAULT);
 
@@ -2986,7 +2985,7 @@ _set_current_bin_to_ready (NleComposition * comp, NleUpdateStackReason reason)
       if (reason != COMP_UPDATE_STACK_ON_SEEK)
         priv->flush_seqnum = gst_event_get_seqnum (flush_event);
       else
-        gst_event_set_seqnum (flush_event, priv->seek_seqnum);
+        gst_event_set_seqnum (flush_event, priv->flush_seqnum);
 
       GST_INFO_OBJECT (comp, "sending flushes downstream with seqnum %d",
           priv->flush_seqnum);
